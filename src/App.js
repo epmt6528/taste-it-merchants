4e5be42d3837ef3ecdad1ea743181b6c9c3d2a01
Antrix <sainiantriksh6@gmail.com>
2020-10-17 17:03:54 -0700

    links connected on home page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React from "react";
 // import logo from './logo.svg';
-import './App.css';
-import Footer from './components/Footer';
-import Header from "./components/Header";
+import "./App.css";
 import Home from "./components/home/Home";
+import SignIn from "./components/restaurants/signIn/SignIn";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ScrollToTop from './components/ScrollToTop'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Header />
-      </header>
-      <Home />
-      <Footer />
-    </div>
+    <Router>
+      <ScrollToTop />
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/signIn" exact component={SignIn} />
+          {/* <Route path="" component={NotFound} />
+          <Route path="*" component={NotFound} />
+          <Route component={NotFound} /> */}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
